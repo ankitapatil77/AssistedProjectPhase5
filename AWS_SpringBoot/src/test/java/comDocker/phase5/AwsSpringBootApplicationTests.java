9cedc4adecd2ed102f066df31921418940b25770
ankitapatil77 <ankitapatil7799@gmail.com>
2022-03-09 23:13:03 +0530

    AWS springboot

---
+++ AWS_SpringBoot/src/test/java/comDocker/phase5/AwsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package comDocker.phase5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
